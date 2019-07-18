bcfecc9bebbf5e0c927de9ebada17aa6556d76a9
Pamplemousse <xav.maso@gmail.com>
2019-07-18 10:07:54 -0700

    [WIP] install on NixOS
      * rely less on pip
      * make use of the packages declared on Pamplemousse/nixpkgs/
      (parents of [this commit](https://github.com/Pamplemousse/nixpkgs/commit/15aa33d43de18fc722c3fc993e479d89833120e4))
    
    TODO:
      * install unicorn properly (not manually)
      * pypy3 is currently broken (need to disable `test_ssl`)

--- setup.sh
+++ setup.sh
@@ -51,13 +51,12 @@ HOMEBREW_DEBS=${HOMEBREW_DEBS-python3 libxml2 libxslt libffi cmake libtool glib 
 ARCHDEBS=${ARCHDEBS-python-virtualenvwrapper python-pip libxml2 libxslt git libffi cmake readline libtool debootstrap glib2 pixman qt5-base binutils binutils nasm lib32-glibc lib32-gcc-libs lib32-zlib lib32-ncurses}
 ARCHCOMDEBS=${ARCHCOMDEBS}
 RPMS=${RPMS-gcc gcc-c++ make python3-virtualenvwrapper python3-pip python3-devel python3-setuptools libxml2-devel libxslt-devel git libffi-devel cmake readline-devel libtool debootstrap debian-keyring glib2-devel pixman-devel qt5-qtdeclarative-devel binutils-x86_64-linux-gnu nasm openssl-devel python2 glibc.i686 libgcc.i686 libstdc++.i686 ncurses-compat-libs.i686 zlib.i686 java-1.8.0-openjdk-devel}
-REPOS=${REPOS-cooldict mulpyplexer monkeyhex archinfo vex pyvex cle claripy angr angr-management angrop angr-doc binaries ailment pysoot}
+REPOS=${REPOS-vex angr angr-management angrop angr-doc binaries}
 declare -A EXTRA_DEPS
 EXTRA_DEPS["angr"]="unicorn"
-EXTRA_DEPS["pyvex"]="--pre capstone"
 
 ORIGIN_REMOTE=${ORIGIN_REMOTE-$(git remote -v | grep origin | head -n1 | awk '{print $2}' | sed -e "s|[^/:]*/angr-dev.*||")}
-REMOTES=${REMOTES-${ORIGIN_REMOTE}angr ${ORIGIN_REMOTE}shellphish ${ORIGIN_REMOTE}mechaphish https://git:@github.com/zardus https://git:@github.com/rhelmot https://git:@github.com/salls https://git:@github.com/lukas-dresel}
+REMOTES=${REMOTES-${ORIGIN_REMOTE}angr ${ORIGIN_REMOTE}shellphish ${ORIGIN_REMOTE}mechaphish https://git:@github.com/zardus https://git:@github.com/rhelmot https://git:@github.com/salls}
 
 
 INSTALL_REQS=0
--- shell.nix
+++ shell.nix
@@ -1,15 +1,39 @@
+# before nix-shell
+# export NIX_PATH="nixpkgs=/home/pamplemousse/Workspace/nixpkgs:${NIX_PATH}"
+
+# TODO: more elegant way to do the following:
+#   install unicorn related stuff:
+# nix-shell -p python2 python3Packages.virtualenvwrapper
+# source $(command -v virtualenvwrapper.sh)
+# workon venv
+# UNICORN_QEMU_FLAGS="--python=$(which python2)" pip install unicorn
+
 with import <nixpkgs> { };
 
-stdenv.mkDerivation rec {
+let myPython3 =
+  python3.withPackages(ps: with ps; [
+    python3Packages.nose
+    python3Packages.ipdbplugin
+    python3Packages.networkx
+
+    # the angr contigent
+    python3Packages.ailment
+    python3Packages.archinfo
+    python3Packages.claripy
+    python3Packages.cle
+    python3Packages.z3-solver
+  ])
+;
+in stdenv.mkDerivation rec {
   name = "angr-env";
 
   nativeBuildInputs = [ cmake pkgconfig git ];
 
   buildInputs = [
     python3Packages.virtualenvwrapper
-    python2   # To build unicorn
-    python3   # For CPython install
-    pypy3     # for PyPy install
+    myPython3 # For CPython install
+    # pypy3     # for PyPy install
+
     nasm
     libxml2
     libxslt
@@ -29,5 +53,6 @@ stdenv.mkDerivation rec {
 
   shellHook = ''
       source $(command -v virtualenvwrapper.sh)
+      [ -d "$HOME/.virtualenvs/venv" ] && workon venv
   '';
 }
